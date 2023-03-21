1299283b4c0a91dfbf08447911ef2b2dd00e1812
Duy Nguyen <duyn@sccwrp.org>
2023-03-21 13:09:11 -0700

    updates to bmp hydrology interface

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(zip)
 library(writexl)
+library(glue)
 
 server <- function(input, output) {
   
@@ -8,8 +9,79 @@ server <- function(input, output) {
 
     if (input$analysistype == 'Rainfall Analysis') {
     
-      output$test <- renderUI({
+      output$user_input <- renderUI({
         tagList(
+          column(
+            4,
+            align = "left",
+            br(),
+            fileInput(
+              "file",
+              "Step 1: Upload an Excel File",
+              multiple = FALSE,
+              accept = ".xlsx"
+            ),
+            selectInput(inputId = "rainfall_resolution",
+                        label = "Step 2: Choose the rainfall resolution",
+                        choices = c("0.01 inch" = 0.01, "0.1 mm" = 0.1),
+                        selected = "0.01 inch"),
+          ),
+          column(
+            4,
+            align = "left",
+            uiOutput("date_column"),
+            uiOutput("rain_column")
+          ),
+          
+          column(
+            4,
+            br(),
+            br(),
+            br(),
+            br(),
+            align = "left",
+            actionButton("submit", "Submit", width = '200px')
+          )
+        )
+      })
+    } else if (input$analysistype == 'Flow Analysis') {
+      output$user_input <- renderUI({
+        tagList(
+          column(
+            4,
+            align = "left",
+            br(),
+            fileInput(
+              "file",
+              "Step 1: Upload an Excel File",
+              multiple = FALSE,
+              accept = ".xlsx"
+            ),
+            selectInput(inputId = "rainfall_resolution",
+                        label = "Step 2: Choose the rainfall resolution",
+                        choices = c("0.01 inch" = 0.01, "0.1 mm" = 0.1),
+                        selected = "0.01 inch"),
+          ),
+          column(
+            4,
+            align = "left",
+            uiOutput("date_column"),
+            uiOutput("flow_column")
+          ),
+          column(
+            4,
+            br(),
+            br(),
+            br(),
+            br(),
+            align = "left",
+            actionButton("submit", "Submit", width = '200px')
+          )
+        )
+      })
+    } else if (input$analysistype == "Both Rainfall and Flow Analysis") {
+
+      output$user_input <- renderUI({tagList(
         column(
           4,
           align = "left",
@@ -36,52 +108,33 @@ server <- function(input, output) {
           4,
           align = "left",
           uiOutput("flow_column"),
-          actionButton("submit", "Submit")
+          br(),
+          br(),
+          actionButton("submit", "Submit", width = '200px')
         )
-        )
-      })
+        
+      )})     
+    }
+  })
+  
+  observe({
+    if(input$analysistype == 'Flow Analysis'){
+      print("true")
+      updateSelectInput(inputId = "flow_column", label = "Step 4: Choose a column containing flow values")
+    }
+  })
+  
+  unit <- reactive({
+    if (input$rainfall_resolution == 0.01){
+      out <- "inch"
     } else {
-      output$test <- renderUI({
-        tagList(
-          column(
-            2,
-          ),
-          column(
-            2,
-            align = "left",
-            br(),
-            fileInput(
-              "file",
-              "Step 1: Upload an Excel File",
-              multiple = FALSE,
-              accept = ".xlsx"
-            ),
-            selectInput(inputId = "rainfall_resolution",
-                        label = "Step 2: Choose the rainfall resolution",
-                        choices = c("0.01 inch" = 0.01, "0.1 mm" = 0.1),
-                        selected = "0.01 inch"),
-          ),
-          column(
-            2,
-            align = "left",
-            uiOutput("date_column"),
-            uiOutput("rain_column")
-          ),
-          
-          # column(
-          #   2,
-          #   align = "left",
-          #   uiOutput("flow_column"),
-          #   actionButton("submit", "Submit")
-          # )
-        )
-      })
+      out <- "mm"
     }
   })
   
   
   ######## tab control based on whether pollutant data is included, updates dynamically
-  tabs_list <- reactiveValues(data = list("Rainfall-Analysis" = NULL, "Flow-Analysis" = NULL))
+  tabs_list <- reactiveValues(data = list("Result" = NULL))
   
   observeEvent(
     input$submit,{
@@ -90,41 +143,37 @@ server <- function(input, output) {
   )
 
   observeEvent(statistics(), {
-
-    if (is.null(tabs_list[['Rainfall-Analysis']])) {
-      insertTab(
-        "full_page",
-        tab = tabPanel(
-          "Rainfall Analysis",
-          
-          fluidRow(
-            column(
-              5,
-              align = "center",
-              br(),
-              DT::dataTableOutput("stats"),
-              br(),
-              downloadButton("download_summary", "Download Table")
-            ),
-            column(
-              5,
-              offset = 1,
-              align = "center",
-              br(),
-              br(),
-              plotOutput("cumulative_rain"),
-              selectInput("choose_graph", "Choose An Event", choices = 1:nrow(statistics()), selected = 1),
-              downloadButton("download_plot", "Download Plot"),
+    if (is.null(tabs_list[['Result']])) {
+        insertTab(
+          "full_page",
+          tab = tabPanel(
+            "Result",
+            fluidRow(
+              column(
+                6,
+                align = "left",
+                br(),
+                DT::dataTableOutput("stats"),
+                br(),
+                downloadButton("download_summary", "Download Table")
+              ),
+              column(
+                5,
+                align = "left",
+                br(),
+                br(),
+                plotOutput("cumulative_rain"),
+                selectInput("choose_graph", "Choose An Event", choices = 1:nrow(statistics()), selected = 1),
+                downloadButton("download_plot", "Download Plot"),
+              )
             )
-          )
-        ),
-        select = TRUE
-      )
-      tabs_list[["Rainfall-Analysis"]] = "Rainfall-Analysis"
-      }
-    removeModal()
+          ),
+          select = TRUE
+        )
+        tabs_list[["Result"]] = "Result"
     }
-  ) 
+    removeModal()
+  }) 
   
   data_input <- reactive({
     rain <- readxl::read_excel(input$file$datapath)
@@ -142,15 +191,15 @@ server <- function(input, output) {
   })
   
   output$date_column <- renderUI({
-    selectInput(inputId = "date_column", label = "Step 3: Choose a column representing datetime", choices = "")
+    selectInput(inputId = "date_column", label = "Step 3: Choose a column containing datetime values", choices = "")
   })
   
   output$rain_column <- renderUI({
-    selectInput(inputId = "rain_column", label = "Step 4: Choose a column representing rainfall data", choices = "")
+    selectInput(inputId = "rain_column", label = "Step 4: Choose a column containing rainfall values", choices = "")
   })
   
   output$flow_column <- renderUI({
-    selectInput(inputId = "flow_column", label = "Step 5: Choose a column representing flow data", choices = "")
+    selectInput(inputId = "flow_column", label = "Step 5: Choose a column containing flow values", choices = "")
   })
   
   
@@ -176,14 +225,14 @@ server <- function(input, output) {
   
   
   observeEvent(input$submit,{
-    updateSelectInput(inputId = "choose_graph", choices = 1:nrow(statistics()), selected = 1)
+    updateSelectInput(inputId = "choose_graph", choices = append(as.character(1:nrow(statistics())), "All events"), selected = "All events")
   }
   )
 
   
   response <- reactive({
     body = payload()
-    res <- httr::POST("https://nexus.sccwrp.org/bioretentionapi/rain", body = body, encode = "json", httr::content_type_json())
+    res <- httr::POST("https://nexus.sccwrp.org/bmp_hydrology/api/rain", body = body, encode = "json", httr::content_type_json())
     content <- httr::content(res)
     content
   })
@@ -223,15 +272,21 @@ server <- function(input, output) {
     
     data <- statistics() |>
       dplyr::select(-last_rain) |>
-      dplyr::mutate(first_rain = format(as.POSIXct(first_rain), format = "%Y-%m-%d %H:%M:%S")) 
+      dplyr::mutate(
+        first_rain = format(as.POSIXct(first_rain), format = "%Y-%m-%d %H:%M:%S"),
+        avg_rainfall_intensity = round(avg_rainfall_intensity, 2),
+        peak_5_min_rainfall_intensity = round(peak_5_min_rainfall_intensity, 2),
+        peak_10_min_rainfall_intensity = round(peak_10_min_rainfall_intensity, 2),
+        
+      ) 
     if (input$rainfall_resolution == 0.1){
       data <- data |> dplyr::rename(
         `Event ID`= event,
         `Storm Date`= first_rain,
         `Total Rainfall (P) (mm)` = total_rainfall,
         `Average Rainfall Intensity (mm/hour)` = avg_rainfall_intensity,
-        `Peak 5-min Rainfall Intensity (mm)` = peak_5_min_rainfall_intensity,
-        `Peak 10-min Rainfall Intensity (mm)` = peak_10_min_rainfall_intensity
+        `Peak 5-min Rainfall Intensity (mm/5-min)` = peak_5_min_rainfall_intensity,
+        `Peak 10-min Rainfall Intensity (2*mm/5-min)` = peak_10_min_rainfall_intensity
         )
     } else {
       data <- data |> dplyr::rename(
@@ -239,8 +294,8 @@ server <- function(input, output) {
         `Storm Date`= first_rain,
         `Total Rainfall (P) (inches)` = total_rainfall,
         `Average Rainfall Intensity (inch/hour)` = avg_rainfall_intensity,
-        `Peak 5-min Rainfall Intensity (inches)` = peak_5_min_rainfall_intensity,
-        `Peak 10-min Rainfall Intensity (inches)` = peak_10_min_rainfall_intensity
+        `Peak 5-min Rainfall Intensity (inches/5-min)` = peak_5_min_rainfall_intensity,
+        `Peak 10-min Rainfall Intensity (2*inches/5-min)` = peak_10_min_rainfall_intensity
       )  
     }
       
@@ -249,30 +304,45 @@ server <- function(input, output) {
   }, selection = 'none')
   
   plotInput <- reactive({
-    cumulative_rain <- data_input() |> 
-      dplyr::mutate(dummy = TRUE) |> 
-      dplyr::left_join(
-        statistics() |> 
-          dplyr::mutate(dummy = TRUE) |> 
-          dplyr::select(dummy, event, first_rain, last_rain), 
-        multiple = 'all'
-      ) |> 
-      dplyr::group_by(event) |>
-      dplyr::filter(
-        datetime >= first_rain, 
-        datetime <= last_rain,
-        event == as.numeric(input$choose_graph)
-      ) |> 
-      dplyr::select(-dummy) |> 
-      dplyr::arrange(datetime) |>
-      dplyr::mutate(
-        cumsum = cumsum(rain),
-        hours = lubridate::time_length(datetime - datetime[1], unit = "hour")
-      )
-    ggplot(cumulative_rain, aes(x = hours, y = cumsum)) +
-      geom_line() + 
-      labs(x = "Hours Elapsed", y = "Cumulative Rainfall")
-  })
+    if (input$choose_graph == "All events"){
+      cumulative_rain <- data_input() |>
+        dplyr::mutate(
+          cumsum = cumsum(rain),
+          hours = lubridate::time_length(datetime - datetime[1], unit = "hour")
+        )
+      ggplot(cumulative_rain, aes(x = hours, y = cumsum)) +
+        geom_line() + 
+        labs(x = "Elapsed hours from the first rain tip", y = glue("Cumulative rainfall ({unit()})"), title = "Total cumulative rainfall")
+      
+    } else {
+      cumulative_rain <- data_input() |> 
+        dplyr::mutate(dummy = TRUE) |> 
+        dplyr::left_join(
+          statistics() |> 
+            dplyr::mutate(dummy = TRUE) |> 
+            dplyr::select(dummy, event, first_rain, last_rain), 
+          multiple = 'all'
+        ) |> 
+        dplyr::group_by(event) |>
+        dplyr::filter(
+          datetime >= first_rain, 
+          datetime <= last_rain,
+          event == as.numeric(input$choose_graph)
+        ) |> 
+        dplyr::select(-dummy) |> 
+        dplyr::arrange(datetime) |>
+        dplyr::mutate(
+          cumsum = cumsum(rain),
+          hours = lubridate::time_length(datetime - datetime[1], unit = "hour")
+        )
+      ggplot(cumulative_rain, aes(x = hours, y = cumsum)) +
+        geom_line() + 
+        labs(x = "Elapsed hours from the first rain tip", y = glue("Cumulative rainfall ({unit()})"), title = "Cumulative rainfall per a rain event")
+      
+    }
+
+    
+    })
   
   output$cumulative_rain <- renderPlot({
     plotInput()
@@ -331,13 +401,7 @@ server <- function(input, output) {
   )
   
   
-  observeEvent(input$submit, {
 
-      shinyjs::show("notice")
-      shinyjs::show("download_summary")
-      shinyjs::show("download_plot")
-
-  })
 
   
   
--- ui.R
+++ ui.R
@@ -6,45 +6,53 @@ ui <- fluidPage(
   
   
   # Application title
-  titlePanel("Rainfall and Flow Analysis"),
+  titlePanel("BMP Hydrology"),
 
-  # Sidebar with a slider input for number of bins 
+  
   fluidRow(
     column(
       1,
       div()
     ),
-    column(2,
-    radioButtons(
-      "analysistype", 
-      "Choose an option:",
-      choices = c("Rainfall Analysis", "Flow Analysis", "Both Rainfall and Flow Analysis")
-    )),
-    column(7,
-           uiOutput("test")
-           
-           )
-   
+    column(
+      2,
+      radioButtons(
+        "analysistype", 
+        "Choose an option:",
+        choices = c("Rainfall Analysis", "Flow Analysis", "Both Rainfall and Flow Analysis")
+      )
+    ),
+    column(
+      7,
+      uiOutput("user_input")
+    )
   ),
     
   tabsetPanel(
     tabPanel(
       "Instructions",
       h3("Using this Application"),
-      "This application will produce a table of summarization of the rain events, and a plot of cumulative rainfall versus elapsed time from the first rain tip.",
+      "This application allows user to choose between 3 different analysis types.",
       HTML("
       <ol>
-         <li>
-          Upload the rainfall data by clicking the 'Browse' button, and clicking the 'Submit' button. The application will generate the rain event table as well as the plot of cumulative rainfall versus elapsed hours.
-        </li>
         <li>
-          Either 1-min data or time-of-tips data can be used to analyze the rain events
+              Rainfall Analysis: When a user uploads rainfall data, the application provides a summary statistics of rainfall, and the plot of cumulative rainfall
         </li>
+        
         <li>
-          Download the demo data by clicking ",), downloadLink("download_demo_1min", label = "here for 1-min data"), HTML("or",), downloadLink("download_demo_tt", label = "here for time of tips data"), HTML("
+              Flow Analysis: When a user uploads flow data, the application provides a summary statistics of flow
         </li>
+        
+        <li>
+              Both Rainfall and Flow Analysis: When a user uploads both rainfall and flow data, the application provides the summary statistics of both rainfall and flow
+        </li>
+        
+        </li>
+      
+          
       </ol>
          "),
+      HTML("Download the demo data by clicking ",), downloadLink("download_demo_1min", label = "here for 1-min data"), HTML("or",), downloadLink("download_demo_tt", label = "here for time of tips data"), HTML(""),
       hr()
     ),
     id='full_page'
